1e3b90cf0d05e4b1cc0a7fdcb4a174cd607174fe
GGnando <nelio1996@gmail.com>
2025-03-01 21:37:40 -0700

    fixed permission os ldd script

--- base_external/package/ldd/aesd-assignments.mk
+++ base_external/package/ldd/aesd-assignments.mk
@@ -14,9 +14,8 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-GGnando.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += misc-modules
 LDD_MODULE_SUBDIRS += scull
-LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
