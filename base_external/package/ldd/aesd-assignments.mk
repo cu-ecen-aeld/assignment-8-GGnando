d98b97edaceff7d5281c1e947c4805173bdef2f4
GGnando <nelio1996@gmail.com>
2025-03-01 18:26:24 -0700

    first attempt ldd modules

---
+++ base_external/package/ldd/aesd-assignments.mk
@@ -0,0 +1,22 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+LDD_VERSION = 0c38ce0e38b1ff0ef85342641092e45224890329
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-GGnando.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
